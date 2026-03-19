120523729a908b421973c14ffd1363be11b03cc1
Paulo Wells <paulowells@gmail.com>
2026-03-19 03:36:34 -0300

    fix image issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,34 +2,38 @@ import profilePic from "@/data/profile_pics/20241222_143920.jpg";
 import Image from "next/image";
 
 export default function Page() {
-	return (
-		<div className="homepage">
+    return (
+        <div className="homepage">
+            <section id="hero" className="hero">
+                <div className="headline">
+                    <h2>Hi. My name is Paulo</h2>
+                </div>
 
-			<section id="hero" className="hero">
-				<div className="headline">
-					<h2>Hi. My name is Paulo</h2>
-				</div>
+                <div className="picture">
+                    <Image
+                        src={profilePic}
+                        alt="Profile Pic"
+                        fill
+                        sizes="20rem"
+                        loading="eager"
+                    />
+                </div>
 
-				<div className="picture">
-					<Image src={profilePic} alt="Profile Pic" fill />
-				</div>
+                <div className="title">
+                    <h3>Web Developer</h3>
+                </div>
+            </section>
 
-				<div className="title">
-					<h3>Web Developer</h3>
-				</div>
-			</section>
-
-			<section id="content" className="content">
-				<nav className="nav">
-					<ul className="nav__menu">
-						<li className="nav__menu__item">Projects</li>
-						<li className="nav__menu__item">CV</li>
-						<li className="nav__menu__item">About me</li>
-						<li className="nav__menu__item">Contact</li>
-					</ul>
-				</nav>
-			</section>
-
-		</div>
-	);
+            <section id="content" className="content">
+                <nav className="nav">
+                    <ul className="nav__menu">
+                        <li className="nav__menu__item">Projects</li>
+                        <li className="nav__menu__item">CV</li>
+                        <li className="nav__menu__item">About me</li>
+                        <li className="nav__menu__item">Contact</li>
+                    </ul>
+                </nav>
+            </section>
+        </div>
+    );
 }
